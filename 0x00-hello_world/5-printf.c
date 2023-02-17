8b0ea3dd8793a6c72a18f48f7782c57bf33ccc9e
samuel2286 <daudasamuel69@gmail.com>
2023-02-17 05:01:51 -0800

    commit updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - printing hello
- *   * Always reurn:(0 success)
- *    */
+ *main - Entry point printing with printf
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
