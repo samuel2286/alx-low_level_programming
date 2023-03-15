1e8bfd7c5a467a568a7d3bd96fef8001909d7b5b
samuel2286 <daudasamuel69@gmail.com>
2023-03-15 10:31:03 -0700

    commit update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,42 @@
 #include "main.h"
-#include<stdio.h>
+
+
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - prints a string with a new line
+ * @str: a pointer given by the main function
+ * Return: 0
  */
-int main(void)
+
+
+
+void _puts(char *str)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+	int i = 0;
+
+
+
+	while (str[i] != '\0')
+
+
+
+
+
+{
+
+	_putchar(str[i]);
+
+	i++;
+
 }
+
+
+
+_putchar(10);
+
+
+
+}
+
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * _puts - Writes string ending with a new line
+ *
+ * Return: Always return 0 sucess
+ */
+int main(void)
+{
+_puts ("\"At the end of the day, my goal was to be the best hacker\"\n\t-Kevin Mitnick");
+return (0);
+}
